7766ac5ec95d70148b518d4652dc445cd685543f
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-10-17 22:13:36 +0530

    Configuring Prettier

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Adopt-me
 
 A very basic project using almost every feature provided by react.
+
+In vs-code
+Go to Settings -> Search for "Format on Save" and check the Prettier -> Search for "Require config" and check the Prettier -> Install the Prettier Extension -> Create .prettierrc file
--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-const Pet = ({name, animal, breed}) => {
-    return (
-        React.createElement(
-            "div",
-            {},         //any attribute will go here like class or id in key-value pair
-            [
-            React.createElement("h1", {} , name),          //it can be multiple as well, as a div can have multiple children
-            React.createElement("h2", {} , animal),
-            React.createElement("h2", {} , breed)
-            ]
-        )
-    )
-}
-
-
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement(
+    "div",
+    {}, //any attribute will go here like class or id in key-value pair
+    [
+      React.createElement("h1", {}, name), //it can be multiple as well, as a div can have multiple children
+      React.createElement("h2", {}, animal),
+      React.createElement("h2", {}, breed),
+    ]
+  );
+};
 
 //its just a stamp, it will be useful only if we'll be use it
 //Everytime App is called it is going to stamp a div and inside it an h1
 const App = () => {
-    return (
-        React.createElement(
-            "div",
-            {},         //any attribute will go here like class or id in key-value pair
-            [
-            React.createElement("h1", {} , "Adopt-Me"),          //it can be multiple as well, as a div can have multiple children
-            React.createElement(Pet , {name: 'Joey', animal: 'Dog', breed: 'Havenese'}),
-            React.createElement(Pet , {name: 'Chandler', animal: 'Bird', breed: 'Cockateail'}),
-            React.createElement(Pet , {name: 'Ross', animal: 'Cat', breed: 'Mixed'}),
-            ]
-        )
-    )
-}
+  return React.createElement(
+    "div",
+    {}, //any attribute will go here like class or id in key-value pair
+    [
+      React.createElement("h1", {}, "Adopt-Me"), //it can be multiple as well, as a div can have multiple children
+      React.createElement(Pet, {
+        name: "Joey",
+        animal: "Dog",
+        breed: "Havenese",
+      }),
+      React.createElement(Pet, {
+        name: "Chandler",
+        animal: "Bird",
+        breed: "Cockateail",
+      }),
+      React.createElement(Pet, { name: "Ross", animal: "Cat", breed: "Mixed" }),
+    ]
+  );
+};
 
 //it'll overwrite anything inside "root" div
-ReactDOM.render(
-    React.createElement(App), document.getElementById('root')
-)
-
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
