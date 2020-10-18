8cc49cb679025c8b365f24c3fd8d26d01f7ebc9c
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-10-18 15:51:51 +0530

    Adding JSX to the project

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ import Pet from "./Pet";
 //its just a stamp, it will be useful only if we'll be use it
 //Everytime App is called it is going to stamp a div and inside it an h1
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, //any attribute will go here like class or id in key-value pair
-    [
-      React.createElement("h1", {}, "Adopt-Me"), //it can be multiple as well, as a div can have multiple children
-      React.createElement(Pet, {
-        name: "Joey",
-        animal: "Dog",
-        breed: "Havenese",
-      }),
-      React.createElement(Pet, {
-        name: "Chandler",
-        animal: "Bird",
-        breed: "Cockateail",
-      }),
-      React.createElement(Pet, { name: "Ross", animal: "Cat", breed: "Mixed" }),
-    ]
+  // return React.createElement(
+  //   "div",
+  //   {}, //any attribute will go here like class or id in key-value pair
+  //   [
+  //     React.createElement("h1", {}, "Adopt-Me"), //it can be multiple as well, as a div can have multiple children
+  //     React.createElement(Pet, {
+  //       name: "Joey",
+  //       animal: "Dog",
+  //       breed: "Havenese",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Chandler",
+  //       animal: "Bird",
+  //       breed: "Cockateail",
+  //     }),
+  //     React.createElement(Pet, { name: "Ross", animal: "Cat", breed: "Mixed" }),
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1>Adopt-Me</h1>
+      <Pet name="Joey" animal="Dog" breed="Havenese" />
+      <Pet name="Chandler" animal="Bird" breed="Cockateail" />
+      <Pet name="Ross" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
 
 //it'll overwrite anything inside "root" div
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
