523b8011bf24b3a02fad1b7cf0c128ed553936b1
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-10-18 21:22:04 +0530

    Babel configuration

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,45 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-export default function Details() {
-  return (
-    <div>
-      <h1>Hii Lol</h1>
-    </div>
-  );
+//arrow functions dont create new contexts
+export default class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Object.assign(oldState, newState)
+  }
+  state = {
+    loading: true,
+  };
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, name, location, description, media, breed } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
